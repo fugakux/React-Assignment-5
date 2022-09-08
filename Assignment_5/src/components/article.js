d677a194da1dd90bff76e46b582b9d157beaef42
xmanraj <107969108+xmanraj@users.noreply.github.com>
2022-09-08 02:24:22 -0700

    5

---
+++ Assignment_5/src/components/article.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import useGetRequest from "./useGetRequest";
+
+const Article = ({ id }) => {
+	
+
+	const {
+		data: article,
+		isLoading,
+		errorMessage,
+	} = useGetRequest("http://localhost:7000/articles/" + id);
+
+	return (
+		
+		<article class="article">
+			
+			<img src={article.image} alt="" />
+			<span className={`topic ${article.topic}`}>{article.topic}</span>
+			<h3>
+				<a href="">{article.headline}</a>
+			</h3>
+			<p>{article.subtext}</p>
+		</article>
+	);
+};
+
+export default Article;
